fcc8960d1e31434f43eb7ef16f701c590cb1b582
IT24103119 <it24103119@my.sliit.lk>
2024-09-02 22:11:25 +0530

    Add files via upload

---
+++ Q2/IT24103119Lab6Q2B.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class IT24103119Lab6Q2B
+{
+	public static void main(String[] args)
+	{
+	Scanner brand = new Scanner(System.in);
+	
+	System.out.println("Enter number 1: ");
+	int D = brand.nextInt();
+	
+	System.out.println("Enter number 2: ");
+	int J = brand.nextInt();
+	
+	System.out.println("Enter number 3: ");
+	int C = brand.nextInt();
+	
+	System.out.println("Enter number 4: ");
+	int O = brand.nextInt();
+	
+	System.out.println("Enter number 5: ");
+	int E = brand.nextInt();
+	
+	System.out.println("Enter number 6: ");
+	int R = brand.nextInt();
+	
+	System.out.println("Enter number 7: ");
+	int S = brand.nextInt();
+	
+	System.out.println("Enter number 8: ");
+	int T = brand.nextInt();
+	
+	System.out.println("Enter number 9: ");
+	int e = brand.nextInt();
+	
+	System.out.println("Enter number 10: ");
+	int d = brand.nextInt();
+
+	System.out.print("The enumbers you entered are : "+D+" "+J+" "+C+" "+O+" "+E+" "+R+" "+S+" "+T+" "+e+" "+d+" ");
+	
+	}
+}
